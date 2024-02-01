8d0310a341445d740e5c0592e08eb79e34471e1d
dLucs <lucas_61@hotmail.es>
2024-02-01 12:24:08 +0100

    Initial Commit

---
+++ SpaceWalk/SpaceWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceWalkApp.swift
+//  SpaceWalk
+//
+//  Created by Lucas on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
